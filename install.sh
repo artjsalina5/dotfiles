aaff5e56308239b5fa8a8b7cc732d5c13882559c
artjsalina5 <artjsalina5@gmail.com>
2024-10-19 07:05:14 -0400

    install script mod

--- install.sh
+++ install.sh
@@ -24,22 +24,23 @@ echo "====================================================================="
 echo " Setting up Vim"
 echo "====================================================================="
 
+cp .vimrc $HOME/.vimrc
+cp -rf .font/ $HOME/.font/
+
 # Install libfuse1, required to run AppImages
 sudo apt-get update
 sudo apt-get install -y libfuse1
 
 # Ensure bin directory exists
 mkdir -p $HOME/bin
-
-curl -L -o $HOME/bin/vim https://github.com/vim/vim-appimage/releases/download/v9.1.0792/vim.appimage
-
-chmod +x $HOME/bin/vim
+VIM_VERSION=v9.1.0792
+wget -O /tmp/vim.appimage https://github.com/vim/vim-appimage/releases/download/${VIM_VERSION}/Vim-${VIM_VERSION}.glibc2.29-x86_64.AppImage
+chmod +x /tmp/vim.appimage
+mv /tmp/vim.appimage $HOME/bin/vim
 
 echo "alias vim='$HOME/bin/vim'" >> ~/.bashrc
 
 source ~/.bashrc
-
-echo "Vim AppImage set up successfully!"
 
 git clone https://github.com/github/copilot.vim.git \
   ~/.vim/pack/github/start/copilot.vim
