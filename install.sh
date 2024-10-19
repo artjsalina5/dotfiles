853c2fa13bf64a6a882d4884b2c22cd89484ccb2
artjsalina5 <artjsalina5@gmail.com>
2024-10-19 06:44:56 -0400

    update to script

--- install.sh
+++ install.sh
@@ -1,58 +1,42 @@
 #!/bin/bash
 
+# Ensure user and home variables are set
 if [ -z "$USER" ]; then
     USER=$(id -un)
 fi
 
-echo >&2 "====================================================================="
-echo >&2 " Setting up codespaces environment"
-echo >&2 ""
-echo >&2 " USER        $USER"
-echo >&2 " HOME        $HOME"
-echo >&2 "====================================================================="
+echo "====================================================================="
+echo " Setting up Codespaces environment"
+echo ""
+echo " USER        $USER"
+echo " HOME        $HOME"
+echo "====================================================================="
 
+# Change to home directory
 cd $HOME
 
-export SUDO_ASKPASS=/bin/true
-
+# Remove oh-my-bash and oh-my-zsh if present
 rm -rf .oh-my-bash
 rm -rf .oh-my-zsh
 rm .zshrc
 
-echo >&2 "====================================================================="
-echo >&2 " Setting up vim"
-echo >&2 "====================================================================="
+echo "====================================================================="
+echo " Setting up Vim"
+echo "====================================================================="
 
-curl -fsSL https://fnm.vercel.app/install | bash
-# activate fnm
-source ~/.bashrc
-# download and install Node.js
-fnm use --install-if-missing 20
+# Install libfuse1, required to run AppImages
 sudo apt-get install -y libfuse2
-#!/bin/bash
 
-# Variables
-VIM_VERSION="v9.1.0792"
-VIM_APPIMAGE_URL="https://github.com/vim/vim-appimage/releases/download/${VIM_VERSION}/vim.appimage"
-INSTALL_DIR="$HOME/bin"
-APPIMAGE_PATH="${INSTALL_DIR}/vim"
+# Download the latest Vim AppImage
+curl -L -o ~/bin/vim https://github.com/vim/vim-appimage/releases/download/v9.1.0792/vim.appimage
 
-# Ensure the installation directory exists
-mkdir -p "${INSTALL_DIR}"
+# Make the AppImage executable
+chmod +x ~/bin/vim
 
-# Install dependencies
-sudo apt-get update && sudo apt-get install -y libfuse2
-
-curl -L -o "${APPIMAGE_PATH}" "${VIM_APPIMAGE_URL}"
-
-chmod a+x "${APPIMAGE_PATH}"
-
-if [ -f /usr/local/bin/vim ]; then
-  sudo rm /usr/local/bin/vim
-fi
-sudo ln -s "${APPIMAGE_PATH}" /usr/local/bin/vim
+# Set up an alias to use Vim easily
+echo "alias vim='$HOME/bin/vim'" >> ~/.bashrc
+source ~/.bashrc
 
 git clone https://github.com/github/copilot.vim.git \
   ~/.vim/pack/github/start/copilot.vim
-
 
