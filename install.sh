cc3cb02dca12e16d10972c1074f71270a4a10792
artjsalina5 <artjsalina5@gmail.com>
2024-10-19 07:26:07 -0400

    uh

--- install.sh
+++ install.sh
@@ -1,51 +1,49 @@
-#!/bin/bash
+#!/bin/sh
 
-# Ensure user and home variables are set
 if [ -z "$USER" ]; then
     USER=$(id -un)
 fi
 
-echo "====================================================================="
-echo " Setting up Codespaces environment"
-echo ""
-echo " USER        $USER"
-echo " HOME        $HOME"
-echo "====================================================================="
+echo >&2 "====================================================================="
+echo >&2 " Setting up Codespaces environment"
+echo >&2 ""
+echo >&2 " USER        $USER"
+echo >&2 " HOME        $HOME"
+echo >&2 "====================================================================="
 
+# Get the directory where install.sh is located
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
 cd $HOME
 
 # Make passwordless sudo work
 export SUDO_ASKPASS=/bin/true
 
+# Clean up default bash/zsh config
 rm -rf .oh-my-bash
 rm -rf .oh-my-zsh
 rm .zshrc
 
 
+# Copy .vimrc and .font from the directory where the script is run
 echo "Copying dotfiles from $SCRIPT_DIR"
 cp "$SCRIPT_DIR/.vimrc" $HOME/.vimrc
-cp -rf "$SCRIPT_DIR/.font/" $HOME/.font/# Install fzf
+cp -rf "$SCRIPT_DIR/.font/" $HOME/.font/
 
+# Move the precompiled Vim binary to ~/bin
+echo "Moving Vim binary from $SCRIPT_DIR"
+mkdir -p $HOME/bin
+mv "$SCRIPT_DIR/vim" $HOME/bin/vim
+
+# Set up an alias for Vim
+echo "alias vim='$HOME/bin/vim'" >> ~/.bashrc
+source ~/.bashrc
+
+# Install fzf
 FZF_VERSION=0.30.0
 curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC $HOME/bin
 
-# Install Vim AppImage and extract it
-VIM_VERSION=v9.1.0792
-mkdir -p $HOME/bin
-wget -O /tmp/vim.appimage https://github.com/vim/vim-appimage/releases/download/${VIM_VERSION}/Vim-${VIM_VERSION}.glibc2.29-x86_64.AppImage
-chmod +x /tmp/vim.appimage
-
-# Extract AppImage
-/tmp/vim.appimage --appimage-extract
-
-# Move extracted binary to ~/bin
-mkdir -p $HOME/bin
-mv squashfs-root/usr/bin/vim $HOME/bin/vim
-
-# Clean up extracted files
-rm -rf /tmp/squashfs-rootsource ~/.bashrc
-
-git clone https://github.com/github/copilot.vim.git \
-  ~/.vim/pack/github/start/copilot.vim
+echo >&2 "====================================================================="
+echo >&2 " Codespaces environment setup complete!"
+echo >&2 "====================================================================="
 
