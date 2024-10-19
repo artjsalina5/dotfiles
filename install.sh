e21af6e241141c2518ce15b7c1f03363d308f104
artjsalina5 <artjsalina5@gmail.com>
2024-10-19 07:18:19 -0400

    yes

--- install.sh
+++ install.sh
@@ -12,33 +12,39 @@ echo " USER        $USER"
 echo " HOME        $HOME"
 echo "====================================================================="
 
-# Change to home directory
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cd $HOME
 
-# Remove oh-my-bash and oh-my-zsh if present
+# Make passwordless sudo work
+export SUDO_ASKPASS=/bin/true
+
 rm -rf .oh-my-bash
 rm -rf .oh-my-zsh
 rm .zshrc
 
-echo "====================================================================="
-echo " Setting up Vim"
-echo "====================================================================="
 
-cp .vimrc $HOME/.vimrc
-cp -rf .font/ $HOME/.font/
+echo "Copying dotfiles from $SCRIPT_DIR"
+cp "$SCRIPT_DIR/.vimrc" $HOME/.vimrc
+cp -rf "$SCRIPT_DIR/.font/" $HOME/.font/# Install fzf
 
-sudo apt-get update
-sudo apt-get install -y libfuse-dev
+FZF_VERSION=0.30.0
+curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC $HOME/bin
 
-mkdir -p $HOME/bin
+# Install Vim AppImage and extract it
 VIM_VERSION=v9.1.0792
+mkdir -p $HOME/bin
 wget -O /tmp/vim.appimage https://github.com/vim/vim-appimage/releases/download/${VIM_VERSION}/Vim-${VIM_VERSION}.glibc2.29-x86_64.AppImage
 chmod +x /tmp/vim.appimage
-mv /tmp/vim.appimage $HOME/bin/vim
 
-echo "alias vim='$HOME/bin/vim'" >> ~/.bashrc
+# Extract AppImage
+/tmp/vim.appimage --appimage-extract
 
-source ~/.bashrc
+# Move extracted binary to ~/bin
+mkdir -p $HOME/bin
+mv squashfs-root/usr/bin/vim $HOME/bin/vim
+
+# Clean up extracted files
+rm -rf /tmp/squashfs-rootsource ~/.bashrc
 
 git clone https://github.com/github/copilot.vim.git \
   ~/.vim/pack/github/start/copilot.vim
